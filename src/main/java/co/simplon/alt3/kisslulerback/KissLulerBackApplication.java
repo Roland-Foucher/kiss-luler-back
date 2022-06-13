3f451e4d39ae823ed0dc5681519f338071cdc9da
Roland Foucher <roland.foucher.pro@gmail.com>
2022-06-13 14:40:27 +0200

    init project

---
+++ src/main/java/co/simplon/alt3/kisslulerback/KissLulerBackApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.alt3.kisslulerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KissLulerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KissLulerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/simplon/alt3/kisslulerback/KissLulerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.alt3.kisslulerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KissLulerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
