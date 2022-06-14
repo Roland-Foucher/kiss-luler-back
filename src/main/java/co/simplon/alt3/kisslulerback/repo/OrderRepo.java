4f3c3069dd98b3569f8aae75f60ee10d8dcef27b
lelia69 <elbouranialcaraz.lelia.simplon@gmail.com>
2022-06-14 16:09:57 +0200

    20-repo-consideration-repo-order done

---
+++ src/main/java/co/simplon/alt3/kisslulerback/repo/OrderRepo.java
@@ -0,0 +1,8 @@
+package co.simplon.alt3.kisslulerback.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import co.simplon.alt3.kisslulerback.entites.Order;
+
+public interface OrderRepo extends JpaRepository <Order, Integer> {
+    
+}
