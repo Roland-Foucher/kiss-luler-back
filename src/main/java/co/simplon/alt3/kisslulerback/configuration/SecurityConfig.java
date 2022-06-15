faf9283c6277bdf24c822b4ea91dc0ff9cee77e4
Roland Foucher <roland.foucher.pro@gmail.com>
2022-06-15 16:39:52 +0200

    start spring security config

--- src/main/java/co/simplon/alt3/kisslulerback/configuration/SecurityConfig.java
+++ src/main/java/co/simplon/alt3/kisslulerback/configuration/SecurityConfig.java
@@ -1,21 +1,59 @@
 package co.simplon.alt3.kisslulerback.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import co.simplon.alt3.kisslulerback.enums.Role;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+
   
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+  {
+
+      http.cors().configurationSource(request -> corsConfiguration());
+
+      http.authorizeRequests()
+          .mvcMatchers("/api/user/account").authenticated()
+          .mvcMatchers("/api/admin/*").hasAuthority(Role.ADMIN.toString())
+          //exemple avec méthode spécifique
+          // .mvcMatchers(HttpMethod.GET, "/api/user").authenticated()
+          .anyRequest().permitAll()
+          .and().csrf().disable()
+          //Changer le comportement pas défaut de la route logout pour lui faire juste renvoyer un 204 (plutôt qu'une redirection)
+          .logout().logoutSuccessHandler((req,res,auth) -> {
+              res.setStatus(204);
+          });
+
+      return http.build();
+  }
 
 
   @Bean
   public PasswordEncoder passwordEncoder() {
       return new BCryptPasswordEncoder(10);
   }
+
+      /**
+     * Cette méthode ne changera à priori pas de projet en projet, ya juste le allowedOrigin
+     * qu'on pourra changer selon où est le front
+     */
+    private CorsConfiguration corsConfiguration() {
+      CorsConfiguration configuration = new CorsConfiguration();
+      
+      configuration.addAllowedOrigin("http://localhost:4200");
+      configuration.addAllowedHeader("*");
+      configuration.addAllowedMethod("*");
+      
+      return configuration;
+  }
+
 }
---
+++ src/main/java/co/simplon/alt3/kisslulerback/security/AuthFilter.java
@@ -0,0 +1,86 @@
+package co.simplon.alt3.kisslulerback.security;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jose.KeyLengthException;
+import com.nimbusds.jose.Payload;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+
+import co.simplon.alt3.kisslulerback.entites.User;
+
+public class AuthFilter extends UsernamePasswordAuthenticationFilter {
+
+  public static String jwtSecret = "mqldsfqsvnqskdnmoezncqmlskdncmlsqdcqmsodnc";
+
+  public AuthFilter(AuthenticationManager authenticationManager) {
+    super(authenticationManager);
+  }
+
+  @Override
+  public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+      throws AuthenticationException {
+
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+
+    if (username == null || password == null) {
+      throw new AuthenticationServiceException("username or password are null - can't auth");
+    }
+
+    return this.getAuthenticationManager()
+        .authenticate(new UsernamePasswordAuthenticationToken(username, password));
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+          Authentication authResult) throws IOException, ServletException {
+
+      try {
+          User user = (User) authResult.getPrincipal();
+          JWTClaimsSet claims = new JWTClaimsSet.Builder()
+                  .subject(user.getEmail())
+                  .issuer(request.getRequestURL().toString())
+                  .claim("role", user.getRole())
+                  .expirationTime(Date.from(Instant.now().plusSeconds(1 * 3600)))
+                  .issueTime(new Date())
+                  .build();
+
+          Payload payload = new Payload(claims.toJSONObject());
+
+          JWSObject jwsObject = new JWSObject(new JWSHeader(JWSAlgorithm.HS256),
+                  payload);
+
+          jwsObject.sign(new MACSigner(jwtSecret));
+          
+          response.addHeader("access_token", jwsObject.serialize());
+      } catch (KeyLengthException e) {
+          e.printStackTrace();
+      } catch (JOSEException e) {
+          e.printStackTrace();
+      }
+  }
+
+}
