35d166dab97b8b217fdd51a323aad663cdf4f7c3
@sauvefanny <fanny.sauve.simplon@gmail.com>
2022-06-15 14:13:28 +0200

    issue closed

---
+++ src/main/java/co/simplon/alt3/kisslulerback/repo/ProjectRepo.java
@@ -0,0 +1,9 @@
+package co.simplon.alt3.kisslulerback.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.simplon.alt3.kisslulerback.entites.Project;
+
+public interface ProjectRepo extends JpaRepository<Project, Integer> {
+
+}
