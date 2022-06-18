2e7242fa6d520f48783d2eb4645e2b4af7db34d2
Roland Foucher <roland.foucher.pro@gmail.com>
2022-06-18 16:43:53 +0200

    modification du comportement des date dans objectDTO pour l'affichage d'un projet finni

--- src/main/java/co/simplon/alt3/kisslulerback/DTO/ProjectDTO.java
+++ src/main/java/co/simplon/alt3/kisslulerback/DTO/ProjectDTO.java
@@ -6,72 +6,73 @@ import java.time.Period;
 import co.simplon.alt3.kisslulerback.enums.Category;
 
 public class ProjectDTO {
-    
-    private int id;
-    private String title;
-    private String userName;
-    private String photo;
-    private Category category;
-    private Double considerations;
-    private String date; //date end - date start
 
-    public int getId() {
-        return id;
+  private int id;
+  private String title;
+  private String userName;
+  private String photo;
+  private Category category;
+  private Double considerations;
+  private String date; // date end - date start
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPhoto() {
+    return photo;
+  }
+
+  public void setPhoto(String photo) {
+    this.photo = photo;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
+  public Double getConsiderations() {
+    return considerations;
+  }
+
+  public void setConsiderations(Double considerations) {
+    this.considerations = considerations;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    if (date.isBefore(LocalDate.now())) {
+      this.date = "Closed";
+    } else {
+      this.date = "J - " + Period.between(LocalDate.now(), date).getDays();
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(String photo) {
-        this.photo = photo;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Double getConsiderations() {
-        return considerations;
-    }
-
-    public void setConsiderations(Double considerations) {
-        this.considerations = considerations;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-
-        this.date =  "J - " + Period.between(LocalDate.now(), date).getDays();
-
-       
-
-    }
+  }
 }
--- src/test/java/co/simplon/alt3/kisslulerback/services/ProjectServiceTest.java
+++ src/test/java/co/simplon/alt3/kisslulerback/services/ProjectServiceTest.java
@@ -38,13 +38,36 @@ public class ProjectServiceTest {
     assertEquals(6000.0, projectService.CalculateAllContribution(project));
   }
 
+  /**
+   * test standard de l'affichage des jours restants
+   */
   @Test
   void FetchPeriodeProject() {
 
     ProjectDTO projectDTO = new ProjectDTO();
-    projectDTO.setDate(LocalDate.of(2022, 06, 25));
+    projectDTO.setDate(LocalDate.now().plusDays(9));
     assertEquals("J - 9", projectDTO.getDate());
 
+  }
+
+  /**
+   * test si le delai est depassé
+   */
+  @Test
+  void FetchPeriodeAfterProject() {
+    ProjectDTO projectDTO = new ProjectDTO();
+    projectDTO.setDate(LocalDate.of(2022, 05, 25));
+    assertEquals("Closed", projectDTO.getDate());
+  }
+
+  /**
+   * test si c'est le dernier jour
+   */
+  @Test
+  void FetchPeriodeProjectToday() {
+    ProjectDTO projectDTO = new ProjectDTO();
+    projectDTO.setDate(LocalDate.now());
+    assertEquals("J - 0", projectDTO.getDate());
   }
 
   @Test
